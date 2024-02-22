36499b78c0abd9b23f47e10ca7c2f1603564c797
ix-pat <frederic@unimore.it>
2024-02-22 11:19:59 +0100

    aggiustato F_print

--- R/main-functions.R
+++ R/main-functions.R
@@ -258,10 +258,37 @@ stat_base <- function(samp,brk){
    } else  {
      j1 <- max(which(br1 <= x))
      j2 <- min(which(br2 >= x2))
-     cat("\\begin{eqnarray*}
-     \\%(",x,"<X<",x2,") &=& (",brk[j1+1],"-",x,")\\times h_{",j1,"}+",paste("f_{",(j1+1):(j2-1),"}\\times 100",collapse="+"),"+(",x2,"-",brk[j2],")\\times h_{",j2-1,"} \\\\
-              &=& (",brk[j1+1]-x,")\\times ",datp$hj[j1],"+",paste("(",datp$fj[(j1+1):(j2-1)],")\\times 100",collapse="+"),"+(",x2-brk[j2],")\\times ",datp$hj[j2]," \\\\
-              &=& ",F.int(x2)-F.int(x),"\\times(100)\\\\
+     c00 <- ifelse(x == brk[j1],
+                   paste0("\\%(",x,"<X<",x2,") &=&  f_{",j1,"}\\times 100+"),
+                   paste0("\\%(",x,"<X<",x2,") &=& (",min(brk[j1+1],x2),"-",x,")\\times h_{",j1,"}+"))
+     c10 <- ifelse(x == brk[j1],
+                   paste0(datp$fj[j1],"\\times 100 +"),
+                   paste0("&=& (",min(brk[j1+1],x2)-x,")\\times ",datp$hj[j1],"+"))
+     c02 <- ifelse(x2 == brk[j2+1],
+                   paste0("f_{",j2,"}\\times 100"),
+                   paste0("(",x2,"-",brk[j2],")\\times h_{",j2,"}"))
+     c12 <- ifelse(x2 == brk[j2+1],
+                   paste0(datp$fj[j2],"\\times 100"),
+                   paste0("(",x2-brk[j2],")\\times ",datp$hj[j2])
+     )
+     if (j1==j2) {
+       c00 <- paste0("\\%(",x,"<X<",x2,") &=& (",min(brk[j1+1],x2),"-",x,")\\times h_{",j1,"}")
+       c01  <- ""
+       c02 <- ""
+       c10 <- paste0("&=& (",min(brk[j1+1],x2)-x,")\\times ",datp$hj[j1],"")
+       c11 <- ""
+       c12 <- ""
+     } else if (j1==(j2-1)){
+       c01  <- ""
+       c11 <- ""
+     } else {
+       c01  <- paste(paste("f_{",(j1+1):(j2-1),"}\\times 100",collapse="+"),"+")
+       c11  <- paste(paste("(",datp$fj[(j1+1):(j2-1)],")\\times 100",collapse="+"),"+")
+     }
+     cat("\\begin{eqnarray*}",
+     c00,c01,c02," \\\\ \n",
+     c10,c11,c12," \\\\ \n",
+              "&=& ",F.int(x2)-F.int(x),"\\times(100)\\\\
      \\#(",x,"< X <",x2,") &=&",(F.int(x2)-F.int(x))*n,"
          \\end{eqnarray*}")
    }
